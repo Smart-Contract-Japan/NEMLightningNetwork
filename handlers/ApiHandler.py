9f1ff1bf85713dd0d8d99fe9aea3a4e1319e4b2d
誠 <makoto1337@hotmail.com>
2015-07-18 20:10:24 +0900

    get server runnable

--- handlers/ApiHandler.py
+++ handlers/ApiHandler.py
@@ -2,24 +2,22 @@
 Distributed under the MIT License, see accompanying file LICENSE.txt
 '''
 import tornado.gen
-import json
-import tornadoredis
+import ujson
 import re
 import tornado.websocket
 import collections
 import zlib
 from itertools import izip_longest
-from handlers.BaseHandler import BaseHandler
 import time
 
-class ChannelHandler(BaseHandler):
+class ChannelHandler(tornado.web.RequestHandler):
 	@tornado.gen.coroutine
 	def get(self):
 		response = yield self.api.getBlocksAfter(int(self.get_argument('height')))
 		self.write(response.body)
 		self.finish()
 		
-class TransactionHandler(BaseHandler):
+class TransactionHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write(zlib.decompress(self.redis_client.zrange('blocks', 0, 2, 'desc')[0]))
 		self.finish()
